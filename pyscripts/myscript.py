b4565b11f9efa69db3269056f633e008444d7730
Kent Hanly <khanly@inghams.com.au>
2026-04-07 18:28:30 +1000

    remove caps

--- pyscripts/myscript.py
+++ pyscripts/myscript.py
@@ -7,7 +7,7 @@ Basic Python script scaffold.
 def main():
     """Main entry point of the script."""
     print("Hello, World!")
-    print("Welcome to DEVASC")
+    print("Welcome to DevAsc")
 
 
 if __name__ == "__main__":
